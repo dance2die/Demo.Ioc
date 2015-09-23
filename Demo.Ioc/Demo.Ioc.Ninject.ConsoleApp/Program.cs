f7fb4dae5e11b111e30757dbe8286bbaa2834a97
dance2die <dance2die@hotmail.com>
2015-09-23 15:47:49 -0400

    Simplest Ninject test (first time using IoC)

---
+++ Demo.Ioc/Demo.Ioc.Ninject.ConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Ninject;
+
+namespace Demo.Ioc.Ninject.ConsoleApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var kernel = new StandardKernel();
+			kernel.Bind<ICreditCard>().To<MasterCard>();
+
+			var shopper = kernel.Get<Shopper>();
+			shopper.Charge();
+
+			Console.Read();
+		}
+	}
+
+	/// <summary>
+	/// http://www.pluralsight.com/courses/inversion-of-control
+	/// </summary>
+	public class Shopper
+	{
+		public ICreditCard CreditCard { get; set; }
+
+		public Shopper(ICreditCard creditCard)
+		{
+			CreditCard = creditCard;
+		}
+
+		public int ChargesForCurrentCard()
+		{
+			return CreditCard.ChargeCount;
+		}
+
+		public void Charge()
+		{
+			Console.WriteLine(CreditCard.Charge());
+		}
+	}
+
+	public interface ICreditCard
+	{
+		string Charge();
+		int ChargeCount { get; }
+	}
+
+	public class MasterCard : ICreditCard
+	{
+		public string Charge()
+		{
+			ChargeCount++;
+			return "Master Card";
+		}
+
+		public int ChargeCount { get; private set; }
+	}
+
+	public class VisaCard : ICreditCard
+	{
+		public string Charge()
+		{
+			ChargeCount++;
+			return "Visa Card";
+		}
+
+		public int ChargeCount { get; private set; }
+	}
+}
