83a592566bdffd5d0de76eaa71912c38fe1886e6
dance2die <dontbothermewithspam@gmail.com>
2015-09-25 11:44:12 -0400

    Frist Unity container test

--- Demo.Ioc/Demo.Ioc.Unity.ConsoleApp/Program.cs
+++ Demo.Ioc/Demo.Ioc.Unity.ConsoleApp/Program.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
 
 namespace Demo.Ioc.Unity.ConsoleApp
 {
+	/// <summary>
+	/// https://msdn.microsoft.com/en-us/library/ff647202.aspx?f=255&MSPPError=-2147217396
+	/// </summary>
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
+			var container = new UnityContainer();
+			container.RegisterType<ICreditCard, MasterCard>();
+			//container.RegisterType<ICreditCard, VisaCard>("visa");
 
+			var shopper = container.Resolve<Shopper>();
+			shopper.Charge();
+
+			Console.Read();
 		}
 	}
 
